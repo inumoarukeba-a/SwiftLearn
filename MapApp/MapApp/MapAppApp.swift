cf0fb3704e6db2ddbec6f14826898fbc1f9f9465
Ataru Nakano <atarunakano@gmail.com>
2021-08-27 08:48:19 +0900

    8章

---
+++ MapApp/MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Ataru Nakano on 2021/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
