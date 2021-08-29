92c42bab206ced9d3dc7306a4fcddb2b806385dc
Ataru Nakano <atarunakano@gmail.com>
2021-08-29 11:46:09 +0900

    9章

---
+++ CardApp/CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by Ataru Nakano on 2021/08/27.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
